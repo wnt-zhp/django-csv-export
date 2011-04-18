295454aeb0545da1ea00cb4569deccec15aeae26
Jacek Bzdak <jbzdak@gmail.com>
2011-04-18 20:05:54 +0200

    1. Added ability to export fields from ModelAdmin.list_display 2. Fixed filtering of csv list (GET parameters were not passed) 3. Added ability to set custom endoding (cause Excel sucks)

--- csvexport/admin.py
+++ csvexport/admin.py
@@ -1,10 +1,41 @@
+import cStringIO
+import codecs
 from datetime import datetime
+from functools import update_wrapper
 from django.contrib import admin
-from django.contrib.admin.util import unquote
 from django.http import HttpResponse
-from django.utils.functional import update_wrapper
 import csv
 import re
+import urllib
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 class CSVExportableAdmin(admin.ModelAdmin):
     csv_export_url = '~csv/'
@@ -15,6 +46,8 @@ class CSVExportableAdmin(admin.ModelAdmin):
        'quotechar': '\\',
        'quoting': csv.QUOTE_MINIMAL,
     }
+    csv_list_fields = False
+    csv_encoding = "utf8"
     change_list_template = 'csvexport/change_list.html'
     
     def get_urls(self):
@@ -46,41 +79,79 @@ class CSVExportableAdmin(admin.ModelAdmin):
         
         response = HttpResponse(mimetype='text/csv')
         response['Content-Disposition'] = 'attachment; filename=%s' % self.csv_get_export_filename(request)
-        writer = csv.writer(response, self.csv_export_dialect, **self.csv_export_fmtparam)
+        writer = UnicodeWriter(response, self.csv_export_dialect, encoding=self.csv_encoding, **self.csv_export_fmtparam)
         writer.writerow(headers)
         for row in qs:
-            csvrow = [f.encode('utf-8') if isinstance(f, unicode) else f for f in [self.csv_resolve_field(row, f) for f in fields]]
+            csvrow = [self.csv_resolve_field(row, f) for f in fields]
             writer.writerow(csvrow)    
         return response
         
     def get_csv_export_fields(self, request):
         """
-        Return a sequence of tuples which should be included in the export.
+
+            Return a sequence of tuples which should be included in the export.
         """
-        fields = [f.name for f in self.model._meta.fields]
-        for relation in self.csv_follow_relations:
-            for field in self.model._meta.get_field_by_name(relation)[0].rel.to._meta.fields:
-                fields.append([relation, field.name])
+        
+        if not self.csv_list_fields:
+
+            fields = [f.name for f in self.model._meta.fields]
+            for relation in self.csv_follow_relations:
+                for field in self.model._meta.get_field_by_name(relation)[0].rel.to._meta.fields:
+                    fields.append([relation, field.name])
+            return fields
+        
+        fields_ = self.list_display
+        fields = []
+        for f in fields_:
+            if f == 'action_checkbox':
+                continue
+            if f == '__unicode__':
+                fields.append(f)
+                continue
+            if "__" in f:
+                fields.append(tuple(f.split('__')))
+            else:
+                fields.append(f)
         return fields
-    
+
     def csv_get_export_filename(self, request):
         ts = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         return '%s_%s_%s_export.csv' % (ts, self.model._meta.app_label, self.model._meta.module_name)
 
     def csv_resolve_field(self, row, fieldname):
-        if isinstance(fieldname, basestring):
-            return getattr(row, fieldname)
-        else:
-            obj = row
-            for bit in fieldname:
-                obj = getattr(obj, bit)
-            return obj
-        
+        def internal():
+            if isinstance(fieldname, basestring):
+                return getattr(row, fieldname)
+            else:
+                obj = row
+                for bit in fieldname:
+                    obj = getattr(obj, bit)
+                return obj
+
+        obj = internal()
+        try:
+            obj = obj()
+        except TypeError:
+            pass
+        if not isinstance(obj, basestring):
+            obj = unicode(obj)
+        return obj
+
+
     def csv_get_fieldname(self, field):
         if isinstance(field, basestring):
             return field
         return '.'.join(field)
+
+    def csv_build_get_string(self, request):
+        params = []
+        for name in request.GET.iterkeys():
+            for x in request.GET.getlist(name):
+                params.append((name, x))
+
+        return u"%s?%s" % (self.csv_export_url, urllib.urlencode(params))
         
+
     def changelist_view(self, request, extra_context=None):
-        extra_context = {'csv_export_url': self.csv_export_url}
+        extra_context = {'csv_export_url': self.csv_build_get_string(request)}
         return super(CSVExportableAdmin, self).changelist_view(request, extra_context)
